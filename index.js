b7ba3d2d326ca7569d7d2e45a84810372f853634
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-02-21 17:47:43 +0530

    update

--- index.js
+++ index.js
@@ -7,9 +7,42 @@ function addtask() {
         mytodosfromLS.push(gettingTodos);
         localStorage.setItem("Todos", JSON.stringify(mytodosfromLS));
         document.getElementById("typetodo").value ="";
+        submit();
         alert("Task added succesfully !");
     }
     else{
         alert('Add todo first');
     }
 }
+
+function submit(){
+    
+    var divFromhtml = document.getElementById("displayTodos");
+
+    var gettingTodosFromLS = JSON.parse(localStorage.getItem("Todos"));
+    console.log(gettingTodosFromLS, "gettingTodosFromLS");
+
+    var finalstructure = [`<div id="row">
+    <p>task</p><p>Completed</p>
+    </div>`]
+
+    for(var i=0; i < gettingTodosFromLS.length; i++ ){
+        finalstructure += `<div id="content">
+        <p>${gettingTodosFromLS[i]}</p>
+        <i onclick="deleteTodo()" class="fa-solid fa-trash"></i>
+        </div>`
+    }
+    divFromhtml.innerHTML = finalstructure;
+}
+submit()
+
+
+function deleteTodo(todoIndex){
+    var todosFromLS = JSON.parse(localStorage.getItem("Todos"));
+    todosFromLS.splice(todoIndex, 1)
+    localStorage.setItem("Todos", JSON.stringify(todosFromLS));  
+    submit(); 
+    alert("Delete Successfully");
+
+}
+
